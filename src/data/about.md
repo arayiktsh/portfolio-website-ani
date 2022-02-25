ad27d770e29f02e97267cfbd22cdc6a3fe944598
AniPetrosyan <ani.petrosyan.y@tumo.org>
2022-02-25 21:14:59 +0400

    ready

---
+++ src/data/about.md
@@ -0,0 +1,6 @@
+
+# Intro
+
+I am the founder of [Girlopolis](https://www.facebook.com/HackathonForGirls) where our team encourages girls in Armenia to pursue a career in IT or business fields. 
+
+
---
+++ src/pages/About.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+import raw from 'raw.macro';
+
+import Main from '../layouts/Main';
+import Cell from '../components/Projects/Cell';
+import data from '../data/interviews';
+
+// uses babel to load contents of file
+const markdown = raw('../data/about.md');
+
+const count = markdown.split(/\s+/)
+  .map((s) => s.replace(/\W/g, ''))
+  .filter((s) => s.length).length;
+
+// Make all hrefs react router links
+const LinkRenderer = ({ ...children }) => <Link {...children} />;
+
+const About = () => (
+  <Main
+    title="About"
+    description="Learn about Ani Petrosyan"
+  >
+    <article className="post markdown" id="about">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/about">About Me</Link></h2>
+          <p>(in about {count} words)</p>
+        </div>
+      </header>
+      <ReactMarkdown
+        source={markdown}
+        renderers={{
+          Link: LinkRenderer,
+        }}
+        escapeHtml={false}
+      />
+    </article>
+
+
+    <article className="post" id="interviews">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/interviews">Interviews</Link></h2>
+          <p>A selection of projects that I&apos;m not too ashamed of</p>
+        </div>
+      </header>
+      {data.map((project) => (
+        <Cell
+          data={project}
+          key={project.title}
+        />
+      ))}
+    </article>
+  </Main>
+);
+
+export default About;
