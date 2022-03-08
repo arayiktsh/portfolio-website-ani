b15bab2eeeeb29071a455d57b61d409917f1413f
AniPetrosyan <ani.petrosyan.y@tumo.org>
2022-03-08 17:59:41 +0400

    updated

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
-import App from './App';
+import { Link } from 'react-router-dom';
 
-// See https://reactjs.org/docs/strict-mode.html
-const StrictApp = () => (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Main from './src/layouts/Main';
+
+const Index = () => (
+  <Main
+    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
+    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+  >
+    <article className="post" id="index">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <p>
+            A beautiful, responsive, statically-generated,
+            react application written with modern Javascript.
+          </p>
+        </div>
+      </header>
+      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+        or you can check out my {' '}
+        <Link to="/resume">resume</Link>, {' '}
+        <Link to="/projects">projects</Link>, {' '}
+        view <Link to="/stats">site statistics</Link>, {' '}
+        or <Link to="/contact">contact</Link> me.
+      </p>
+      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+    </article>
+  </Main>
 );
 
-const rootElement = document.getElementById('root');
-
-// hydrate is required by react-snap.
-if (rootElement.hasChildNodes()) {
-  hydrate(<StrictApp />, rootElement);
-} else {
-  render(<StrictApp />, rootElement);
-}
+export default Index;
